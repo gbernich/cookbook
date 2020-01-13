cb62aa4a88bc9c1a6aa56df9aa8b26df58ea28a1
Garrett Bernichon <bernichg@gmail.com>
2020-01-12 22:30:09 -0500

    Loading all recipes on init.

--- html/index.php
+++ html/index.php
@@ -3,25 +3,42 @@
 	<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 	<meta content="utf-8" http-equiv="encoding">
 </head>
-<!--
-<script>
-function showHint(str) {
-    if (str.length == 0) {
-        document.getElementById("txtHint").innerHTML = "";
-        return;
-    } else {
+
+<script type="text/javascript">
+    function initPage() {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
+                document.getElementById("recipeTable").innerHTML = this.responseText;
             }
         };
-        xmlhttp.open("GET", "gethint.php?q=" + str, true);
+        xmlhttp.open("GET", "showAllRecipes.php", true);
         xmlhttp.send();
     }
+    window.onload = initPage;
+</script>
+
+<script>
+function updateTable() {
+	console.log("updateTable");
+//        document.getElementById("recipeTable").innerHTML = "<p>Hi</p>";
+
+//    if (str.length == 0) {
+//        document.getElementById("recipeTable").innerHTML = "";
+//        return;
+//    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("recipeTable").innerHTML = this.responseText;
+            }
+        };
+        //xmlhttp.open("GET", "findRecipes.php?compliance=" + str, true);
+        xmlhttp.open("GET", "findRecipes.php", true);
+        xmlhttp.send();
+//    }
 }
 </script>
--->
 </head>
 <body>
 <?php
@@ -47,13 +64,14 @@ if ($result->num_rows > 0) {
     echo "<table>";
     echo "<tr>";
     echo "<td></td>";
-    echo "<td>name</td>";
+    echo "<td>Compliance</td>";
     echo "</tr>";
 
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr>";
-        echo "<td><input type=\"checkbox\" id=\"compliance_.$row[id]\"></td>";
+//        echo "<td><input type=\"checkbox\" id=\"compliance_.$row[id]\" onclick=\"updateTable(\"\")\"></td>";
+        echo "<td><input type=\"checkbox\" id=\"compliance_.$row[id]\" onclick=\"updateTable()\"></td>";
         echo "<td>".$row[name]."</td>";
         echo "</tr>";
     }
@@ -65,6 +83,10 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?> 
+
+<br></br>
+
+<div id="recipeTable"></div>
 
 </body>
 </html> 
