a2fe1f60e588f6b658979e58018241faa97d76b7
Garrett Bernichon <bernichg@gmail.com>
2020-01-14 22:07:11 -0500

    Fully populating Recipe entry. Need to handle ingredients and instructions.

--- html/addRecipe.php
+++ html/addRecipe.php
@@ -20,7 +20,11 @@ $sql = "INSERT INTO Recipe (name, description, prep_time, cook_time, total_time,
                                 ".$_POST['prep_time'].",
                                 ".$_POST['cook_time'].",
                                 ".$_POST['total_time'].",
-                               '".$_POST['hot_cold']."', true, true, false, 'LUNCH');";
+                               '".$_POST['hot_cold']."',
+                                ".$_POST['compliance_whole30'].",
+                                ".$_POST['compliance_meatless'].",
+                                ".$_POST['compliance_other'].",
+                               '".$_POST['meal_type']."');";
 
 
 $result = $conn->query($sql);
@@ -36,12 +40,32 @@ $result = $conn->query($sql);
 <body>
 <form method="post" action="">
 	<input type="text" name="name" placeholder="Recipe Name" required><br>
+
 	<textarea cols="40" placeholder="Recipe Description" rows="8" name="description" required></textarea><br>
+
 	<input type="text" name="prep_time" placeholder="Prep Time" ><br>
-	<input type="text" name="cook_time" placeholder="Cook Time" ><br>
-	<input type="text" name="total_time" placeholder="Total Time" ><br>
+
+	<input type="text"  name="cook_time"  placeholder="Cook Time" ><br>
+
+	<input type="text"  name="total_time" placeholder="Total Time" ><br>
+
 	<input type="radio" name="hot_cold" value="HOT" checked /> <span>Hot</span>
-	<input type="radio" name="hot_cold" value="COLD" /> <span>Cold</span>
+	<input type="radio" name="hot_cold" value="COLD"        /> <span>Cold</span><br>
+
+	<input type="radio" name="compliance_whole30"  value="true"          /> <span>WHOLE30</span>
+	<input type="radio" name="compliance_whole30"  value="false" checked /> <span>No</span><br>
+
+	<input type="radio" name="compliance_meatless" value="true"          /> <span>Meatless</span>
+	<input type="radio" name="compliance_meatless" value="false" checked /> <span>No</span><br>
+
+	<input type="radio" name="compliance_other"    value="true"          /> <span>Other</span>
+	<input type="radio" name="compliance_other"    value="false" checked /> <span>No</span><br>
+
+	<input type="radio" name="meal_type"    value="BREAKFAST"  checked /> <span>Breakfast</span>
+	<input type="radio" name="meal_type"    value="LUNCH"              /> <span>Lunch</span>
+	<input type="radio" name="meal_type"    value="DINNER"             /> <span>Dinner</span>
+	<input type="radio" name="meal_type"    value="DESSERT"            /> <span>Dessert</span><br>
+
 	<button type="submit" name="submit">Submit</button>
 </form>
 </body>
