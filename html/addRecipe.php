51a9fa357b70e67b4e2cdacc82c60d82f1c32b0a
Garrett Bernichon <bernichg@gmail.com>
2020-01-14 21:36:32 -0500

    Passing all 3 times to database.

--- html/addRecipe.php
+++ html/addRecipe.php
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
 }
 
 $sql = "INSERT INTO Recipe (name, description, prep_time, cook_time, total_time, hot_cold, compliance_whole30, compliance_meatless, compliance_other, meal_type)
-			VALUES('".$_POST['name']."', '".$_POST['description']."', 5, 15, 20, 'COLD', true, true, false, 'LUNCH');";
+			VALUES('".$_POST['name']."', '".$_POST['description']."', ".$_POST['prep_time'].", ".$_POST['cook_time'].", ".$_POST['total_time'].", 'COLD', true, true, false, 'LUNCH');";
 
 
 $result = $conn->query($sql);
@@ -32,6 +32,9 @@ $result = $conn->query($sql);
 <form method="post" action="">
 	<input type="text" name="name" placeholder="Recipe Name" required><br>
 	<textarea cols="40" placeholder="Recipe Description" rows="8" name="description" required></textarea><br>
+	<input type="text" name="prep_time" placeholder="Prep Time" ><br>
+	<input type="text" name="cook_time" placeholder="Cook Time" ><br>
+	<input type="text" name="total_time" placeholder="Total Time" ><br>	
 	<button type="submit" name="submit">Submit</button>
 </form>
 </body>
